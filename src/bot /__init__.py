1731635ad9bd3101a7d23839f01cd866b81be49b
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-02 12:40:51 +0530

    Delete src/bot  directory

--- src/bot /__init__.py
+++
@@ -1,5 +0,0 @@
-from .bot import Bot  # Importing the Bot class from bot.py
-
-__all__ = ['Bot']  # Specifying what should be available when importing the package
-
-# You can add other initializations or configurations here if needed.
